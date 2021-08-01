54d245c008f83932cbbcba98e36c9968562b4215
clc <1972207371@qq.com>
2021-08-01 11:47:36 +0800

    :sparkles: 17_provide_inject

---
+++ src/components/Child.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="child">
+    <h3>我是Child组件（子）</h3>
+    <Son></Son>
+  </div>
+</template>
+
+<script>
+  import Son from './Son.vue';
+export default {
+  name:'Child',
+  components:{
+    Son
+  }
+}
+</script>
+
+<style>
+.child{
+  background: skyblue;
+  text-align: center;
+  padding: 10px 20px;
+}
+</style>
